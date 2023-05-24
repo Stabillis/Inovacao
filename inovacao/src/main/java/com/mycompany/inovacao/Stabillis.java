5ecb8fbab73b7bef6c6379bd3ea15143c55cd8bc
VictorEmanuelGomes <111135518+VictorEmanuelGomes@users.noreply.github.com>
2023-05-24 20:36:18 -0300

    Inovação completa

--- inovacao/src/main/java/com/mycompany/inovacao/Stabillis.java
+++ inovacao/src/main/java/com/mycompany/inovacao/Stabillis.java
@@ -23,41 +23,38 @@ public class Stabillis {
         List<Processo> nomeProcessos = looca.getGrupoDeProcessos().getProcessos();
 
         
-                  //Inovação
-                 for (int i = 0; i < nomeProcessos.size(); i++) {
-                    if(nomeProcessos.get(i).getUsoCpu()> 75 || nomeProcessos.get(i).getUsoMemoria() > 90){
-                System.out.println(nomeProcessos.get(i));
-            int option = JOptionPane.showOptionDialog(
-                null, // Componente pai (null indica que não temos um componente pai)
-                "Escolha uma opção", // Mensagem exibida no pop-up
-                "Pop-up com Botões", // Título da janela do pop-up
-                JOptionPane.YES_NO_OPTION, // Tipo de botões (SIM e NÃO)
-                JOptionPane.QUESTION_MESSAGE, // Tipo de ícone (interrogação)
-                null, // Ícone personalizado (null indica que usaremos o padrão)
-                new Object[]{"Botão 1", "Botão 2"}, // Texto dos botões
-                "Botão 1" // Botão padrão selecionado
-        );
+                //Inovação
+                    for (int i = 0; i < nomeProcessos.size(); i++) {
+                    if (nomeProcessos.get(i).getUsoCpu() > 75 || nomeProcessos.get(i).getUsoMemoria() > 90) {
+                        System.out.println(nomeProcessos.get(i));
+                        int option = JOptionPane.showOptionDialog(
+                                null, // Componente pai (null indica que não temos um componente pai);;
+                                String.format("O processo %s está em estado critico CPU em %.2f e Memoria em %.2f", nomeProcessos.get(i).getNome(), nomeProcessos.get(i).getUsoCpu(), nomeProcessos.get(i).getUsoMemoria()), // Mensagem exibida no pop-up
+                                "Stabillis", // Título da janela do pop-up
+                                JOptionPane.YES_NO_OPTION, // Tipo de botões (SIM e NÃO)
+                                JOptionPane.ERROR_MESSAGE, // Tipo de ícone (interrogação)
+                                null, // Ícone personalizado (null indica que usaremos o padrão)
+                                new Object[]{"Encerrar o processo", "Não encerrar o processo"}, // Texto dos botões
+                                "Botão 1" // Botão padrão selecionado
+                        );
 
-        // Verifica qual botão foi selecionado
-        if (option == JOptionPane.YES_OPTION) {
-            // Código para o Botão 1
-            
-            try {
-                System.out.println(Runtime.getRuntime().exec("kill -9 7073"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println("Obrigado! Processo encerrado.");
-            
-            
-        } else if (option == JOptionPane.NO_OPTION) {
-            // Código para o Botão 2
-            System.out.println("Obrigado! Processo não encerrado.");
-        }
-    }
-                
-            }
-        }
+                        // Verifica qual botão foi selecionado
+                        if (option == JOptionPane.YES_OPTION) {
+                            // Código para o Botão 1
+                            try {
+                                System.out.println(Runtime.getRuntime().exec("kill -9 " + nomeProcessos.get(i).getPid()));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            JOptionPane.showMessageDialog(null, "Sucesso! Processo encerrado.");
+
+                        } else if (option == JOptionPane.NO_OPTION) {
+                            // Código para o Botão 2
+                            JOptionPane.showMessageDialog(null, "Ok, você optou  por não encerrar o processo. Obrigado!");
+                        }
+                    }
+                }
+                    }
 
     }
 
